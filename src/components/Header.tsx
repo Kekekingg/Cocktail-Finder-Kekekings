173e1d0f52f55bad0a330db069eec43516760aff
Kekeking <erikmreyesm@gmail.com>
2026-08-29 02:50:05 -0600

    feat: implement header with logo, nav links (Home/Favorites) using NavLink; added conditional form on Home with Tailwind styles, input for ingredient, category select placeholder, and search button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,15 @@
-import { Link, NavLink } from "react-router-dom"
+import path from "node:path"
+import { useMemo } from "react"
+import { Link, NavLink, useLocation } from "react-router-dom"
 
 export default function Header() {
+
+    const {pathname} = useLocation()
+
+    const isHome = useMemo(() => pathname === '/' ,[pathname])
+
+    
+
   return (
     <header className="bg-slate-800">
         <div className="mx-auto container px-5 py-16">
@@ -25,8 +34,46 @@ export default function Header() {
                     >Favorites</NavLink>
                 </nav>
             </div>
-        </div>
 
+            {isHome && (
+                <form className="sm:w-1/2 md:w-1/2 2xl:1/3 bg-orange-400 my-20 p-10 rounded-lg shadow space-y-6">
+                    <div>
+                        <label
+                            htmlFor="ingredient"
+                            className="block text-white uppercase font-extrabold text-lg mb-2"
+                            >
+                                Name or Ingredients:
+                        </label>
+
+                        <input
+                            id="ingredient"
+                            type="text" 
+                            name="ingredient"
+                            className="p-3 w-full rounded-lg focus:outline-none bg-white"
+                            placeholder="Name or Ingredient. Ex. Vodka, Tequila, Coffee"
+                        />
+                    </div>
+
+                    <div>
+                        <label
+                            htmlFor="ingredient"
+                            className="block text-white uppercase font-extrabold text-lg mb-2"
+                            >
+                                Category:
+                        </label>
+
+                        <select
+                            id="ingredient" 
+                            name="ingredient"
+                            className="p-3 w-full rounded-lg focus:outline-none bg-white"
+                        >
+                            <option value="" disabled selected>--Select--</option>
+                        </select>
+                    </div>
+                    <input type="submit" value="Search recipes" className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase"/>
+                </form>
+            )}
+        </div>
     </header>
   )
 }
